e79651a1620886e26e014f485d6310a7d6f9fda6
rienihil <aluakural000@gmail.com>
2024-04-05 10:53:59 +0600

    finished problem 2, replaced parameter n with array.length

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         for(int i=0; i<n; i++){
             array[i]=sc.nextInt();
         }
-        System.out.println(minOfArr(n, array, array[0], 0));
+        System.out.println("minOfArr: " + minOfArr(array, 0, array[0]));
+        System.out.println("avgOfArr: " + avgOfArr(array, 0, array[0]));
     }
 
     /**
@@ -17,21 +18,40 @@ public class Main {
      * the method iterates through elements of the array from 0 to n,
      * resulting in linear time complexity
      *
-     * @param n the size of the array
      * @param array the given array
+     * @param index the index of the element being compared
      * @param min the current minimum for comparison
-     * @param index the element being compared
      * @return the minimum of the given array
     */
-    public static int minOfArr(int n, int[] array, int min, int index){
-        if (index == n){
+    public static int minOfArr(int[] array, int index, int min){
+        if (index == array.length){
             return min;
         }
         else{
             if (min>array[index]){
                 min=array[index];
             }
-            return minOfArr(n, array, min, index+1);
+            return minOfArr(array,index+1, min);
+        }
+    }
+
+    /**
+     * this method returns the average of the array using recursion
+     * time complexity: O(n)
+     * the method iterates through elements of the array from 0 to n,
+     * resulting in linear time complexity
+     *
+     * @param array the given array
+     * @param index the index of the current element
+     * @param sum the sum of elements up to index
+     * @return the average of the given array
+     */
+    public static double avgOfArr(int[] array, int index, double sum){
+        if (index+1 == array.length){
+            return sum/array.length;
+        }
+        else{
+            return avgOfArr(array, index+1, sum+array[index+1]);
         }
     }
 
