ed80c3f8d4b29b91f8e6b134cc1414e543d16656
rienihil <aluakural000@gmail.com>
2024-04-05 12:22:16 +0600

    finished problem 6

---
+++ src/problem6.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(power(a,n));
+    }
+
+    /**
+     * this method calculates the given number to the given power
+     * it uses a recursive approach
+     * time complexity: O(n), where n is the given power
+     * the method iterates through numbers from n to 0,
+     * resulting in linear time complexity
+     *
+     * @param a the given number
+     * @param n the given power
+     * @return the given number to the given power
+     */
+    public static int power(int a, int n){
+        if (n==0){
+            return 1;
+        }
+        else if (n==1){
+            return a;
+        }
+        else{
+            return a*power(a,n-1);
+        }
+    }
+}
